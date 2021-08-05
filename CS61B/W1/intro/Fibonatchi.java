2abb69aa4aa00c2fa421a41376c929a061b95b9c
elaraby elaidy <elarabyelaidy@gmail.com>
2021-08-05 05:44:06 +0200

    fibo2 numbers

--- CS61B/W1/intro/Fibonatchi.java
+++ CS61B/W1/intro/Fibonatchi.java
@@ -8,10 +8,20 @@ public class Fibonatchi {
     }
   }
 
+  public static int fibo2(int n, int k,int f0, int f1 ) {
+     
+    if (n == k) { 
+       return f0; 
+    } else { 
+       return fibo2(n, k + 1, f1, f0 + f1);
+    }
+  }
+
 
   public static void main(String[] args) {
-    int n = 7;
-    System.out.println(fibo(n));
+    int n = 9;
+    int f0 = 7; int f1 = 8; int k = 7;
+    System.out.println(fibo2(n, k, f0, f1));
   }
     
 }
