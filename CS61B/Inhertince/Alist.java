48fccd70b6c4c7e3186bb245de0438c5ee4dff9b
elaraby elaidy <elarabyelaidy@gmail.com>
2021-11-13 22:32:50 +0200

    Alist implementatiosn

--- CS61B/Inhertince/Alist.java
+++ CS61B/Inhertince/Alist.java
@@ -1,5 +1,80 @@
-public class Alist {
-    // implement Alist  
-    
+public class Alist<Item> implements List<Item> {
+    private Item[] items;
+    private int size;
+
+    /** Creates an empty list. */
+    public Alist() {
+        items = (Item[]) new Object[100];
+        size = 0;
+    }
+
+    /**
+     * Inserts item into given position. Code from discussion #3
+     */
+    @Override
+    public void insert(Item x, int position) {
+        Item[] newItems = (Item[]) new Object[items.length + 1];
+
+        System.arraycopy(items, 0, newItems, 0, position);
+        newItems[position] = x;
+
+        System.arraycopy(items, position, newItems, position + 1, items.length - position);
+        items = newItems;
+    }
+
+    /** Resizes the underlying array to the target capacity. */
+    private void resize(int capacity) {
+        Item[] a = (Item[]) new Object[capacity];
+        System.arraycopy(items, 0, a, 0, size);
+        items = a;
+    }
+
+    /** Inserts an item at the front. */
+    @Override
+    public void addFirst(Item x) {
+        insert(x, 0);
+    }
+
+    /** Inserts X into the back of the list. */
+    @Override
+    public void addLast(Item x) {
+        if (size == items.length) {
+            resize(size + 1);
+        }
+
+        items[size] = x;
+        size = size + 1;
+    }
+
+    /** Gets an item from the front. */
+    @Override
+    public Item getFirst() {
+        return get(0);
+    }
+
+    /** Returns the item from the back of the list. */
+    public Item getLast() {
+        return items[size - 1];
+    }
+
+    /** Gets the ith item in the list (0 is the front). */
+    public Item get(int i) {
+        return items[i];
+    }
+
+    /** Returns the number of items in the list. */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Deletes item from back of the list and returns deleted item.
+     */
+    public Item removeLast() {
+        Item x = getLast();
+        items[size - 1] = null;
+        size = size - 1;
+        return x;
+    }
 
 }
