b22f71cca1d3d960447fd7d8e9a311850ad6935b
elarabyelaidy19 <elarabyelaidy@gmail.com>
2021-11-18 14:24:25 +0200

    Fill Grid

---
+++ CS61B/Discs/Disc4/IntList.java
@@ -0,0 +1,27 @@
+public class IntList {
+    public int first; 
+    public IntList rest; 
+    
+    public IntList(IntList r, int f) { 
+        this.f = first; 
+        this.r = rest; 
+    }
+    
+    public static void evenOdd(IntList lst){ 
+        if(lst == nulll || lst.rest == nulll) { 
+            return;
+        }
+        // lst point to even pointer
+        IntList odds = lst.rest; 
+        IntList second = lst.rest;
+        while(lst.rest != null && odds.rest != null) { 
+            lst.rest = lst.rest.rest; 
+            odds.rest = odds.rest.rest; 
+            lst = lst.rest; 
+            odds = odds.rest;
+        }
+        lst.rest = second;
+
+
+    }
+}
