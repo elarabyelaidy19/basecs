5822e192e4debfea731b3be3c1b781d594b6b473
elarabyelaidy19 <elarabyelaidy@gmail.com>
2021-11-26 00:16:36 +0200

    Yet Another Dynamic Method Selection

--- CS61B/Discs/Disc7/Q2/TestPeople.java
+++ CS61B/Discs/Disc7/Q2/TestPeople.java
@@ -1,8 +1,9 @@
+package Disc7.Q2;
 public class TestPeople {
     public static void main(String[] args) {
     
         Person n = new Person("neil", 23); 
-        Person a = new Person("ada", 22); 
+        Person a = new GrandMa("ada", 22); 
 
         GrandMa v = new GrandMa("vidya", 80); 
         // GrandMa a1 = new Person("alex", 27); // Compile error can not put Person in GrandMa
@@ -11,12 +12,12 @@ public class TestPeople {
         n.greet(v); // Hello vidya
 
         v.greet(a); // hello young whippersnapper
-        v.greet((GrandMa) a); // how was bingo
+        v.greet((GrandMa) a); // how was bingo, ada?
 
-        a.greet(n); 
-        a.greet(v);
+        a.greet(n); // hello young whippersnapper
+        a.greet(v); // hello young whippersnapper
 
-        ((GrandMa) a).greet(v); 
-        ((GrandMa) n).greet(v);
+        ((GrandMa) a).greet(v); // how wa bingo 
+        ((GrandMa) n).greet(v);// Run time error
     } 
 }
