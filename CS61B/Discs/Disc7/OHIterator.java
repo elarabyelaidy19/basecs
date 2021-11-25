71b7019e246bc888234b27c8d29d07cb6fd9baf3
elarabyelaidy19 <elarabyelaidy@gmail.com>
2021-11-25 23:17:38 +0200

    Office Hour Queue

--- CS61B/Discs/Disc7/OHIterator.java
+++ CS61B/Discs/Disc7/OHIterator.java
@@ -37,9 +37,4 @@ public class OHIterator implements Iterator<OHRequest> {
 
     }
 
-    
-
-
-
-    
 }
--- CS61B/Discs/Disc7/OHRequest.java
+++ CS61B/Discs/Disc7/OHRequest.java
@@ -5,7 +5,7 @@ public class OHRequest {
     public String description; 
     public OHRequest next; 
 
-    public OHRequest(String name, String description) {
+    public OHRequest(String name, String description, OHRequest next) {
         this.name = name; 
         this.description = description; 
         this.next = next; 
