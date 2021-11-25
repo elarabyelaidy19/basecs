50dafa60ff5679cb8748553ca433681d049b3c61
elarabyelaidy19 <elarabyelaidy@gmail.com>
2021-11-25 23:04:00 +0200

    Disc7 2

---
+++ CS61B/Discs/Disc7/OHRequest.java
@@ -0,0 +1,13 @@
+package Disc7;
+
+public class OHRequest {
+    public String name; 
+    public String description; 
+    public OHRequest next; 
+
+    public OHRequest(String name, String description) {
+        this.name = name; 
+        this.description = description; 
+        this.next = next; 
+    }
+}
