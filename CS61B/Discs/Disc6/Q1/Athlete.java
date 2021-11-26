a80837cb21d110108164c7f0dea2473d2f45b4f9
elarabyelaidy19 <elarabyelaidy@gmail.com>
2021-11-27 00:14:49 +0200

    Yet Another Dynamic Method Selection

--- CS61B/Discs/Disc6/Q1/Athlete.java
+++ CS61B/Discs/Disc6/Q1/Athlete.java
@@ -1,3 +1,5 @@
+package Disc6.Q1;
+
 public class Athlete extends Person {
     public void speakTo(Athlete other) { System.out.println("Take notes"); } 
     public void watch(Athlete other) { System.out.println("game on"); } 
