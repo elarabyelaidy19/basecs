2f93a1fd8bff101dfc9a06d127c7efbf29f2f2cc
elarabyelaidy19 <elarabyelaidy@gmail.com>
2021-12-07 13:16:13 +0200

    BST Remove

--- CS61B/Lectures/ADTs.java/BST.java
+++ CS61B/Lectures/ADTs.java/BST.java
@@ -1,4 +1,4 @@
-private class BST<Key> {
+public class BST<Key> {
     private Key key; 
     private BST left; 
     private BST right; 
@@ -37,5 +37,31 @@ private class BST<Key> {
             T.right = insert(T.right, ik); 
         return T;
     }
+
+    public static BST remove(BST T, int k) {
+        if (T == null)return null;
+        if (k < T.label) 
+            T.left = remove(T.left, L);
+        else if (k > T.label)
+            T.right = remove(T.right, L);
+        // Otherwise, we’ve found k
+        else if (T.left == null) 
+            return T.right;
+        else if (T.right == null)
+            return T.left;
+        else T.right = swapSmallest(T.right, T);
+        return T; }
+        /** Move the labek from the first node in T (in an inorder*  traversal) to node R (over-writing the current labek of R),
+         * *  remove the first node of T from T, and return the resulting tree.*/
+        private static BST swapSmallest(BST T, BST R) {
+            if (T.left == null) {R.label = T.label;
+                return T.right;} 
+            else { 
+                T.left = swapSmallest(T.left, R);
+            return T;
+        }
+        }
+
+
 
 }
