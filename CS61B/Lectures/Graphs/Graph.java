4cd07fd4e63c820dc7eedc379d91c9392f7c2768
elarabyelaidy19 <elarabyelaidy@gmail.com>
2021-12-28 23:44:33 +0200

    graph class

--- CS61B/Lectures/Graphs/Graph.java
+++ CS61B/Lectures/Graphs/Graph.java
@@ -1,3 +1,4 @@
+package UCB.CS61B.Lectures.Graphs;
 public class graph { 
     private static final String NEW_LINE = System.getProperty("line.separator"); 
     private final int V; 
@@ -38,7 +39,59 @@ public class graph {
         }
     } 
 
-    
+
+    public int V() { 
+        return V; 
+    } 
+
+    public in E() { 
+        return E; 
+    }
+
+    private void validateVertex(int v) { 
+        if(v < 0 || v >= V) throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    public void addEdge(int v, int w) { 
+        validateVertex(v); 
+        validateVertex(x); 
+        E++; 
+        adj[v].add(w); 
+        adj[w].add(v);
+    }
+
+
+    public Iterable<Integer> adj(int v) { 
+        validateVertex(v); 
+        return adj[v]; 
+    }
+
+
+    public int degree(int v) { 
+        validateVertex(v); 
+        return adj[v].size(); 
+    }
+
+    public String toString() { 
+        StringBuilder s = new StringBuilder(); 
+        a.append(V + "vertices, " + E + "edges " + NEW_LINE); 
+        for(int v = 0; v < V; v++) { 
+            s.append(v + ": "); 
+            for(int w : adj[v]) { 
+                s.append(w + " "); 
+
+            }
+            s.append(NEW_LINE);
+        }
+        return s.toString();
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Graph G = new Graph(in);
+        StdOut.println(G);
+    }
+
 
 
 
