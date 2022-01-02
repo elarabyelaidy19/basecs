c56aa3e6670b2808de457b64698780f575345905
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-01-02 23:09:50 +0200

    Depth first Path

--- CS61B/Lectures/Graphs/DepthFirstPath.java
+++ CS61B/Lectures/Graphs/DepthFirstPath.java
@@ -48,6 +48,28 @@ public class DepthFirstPath {
         }
     }
 
-    
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Graph G = new Graph(in);
+        int s = Integer.parseInt(args[1]);
+        DepthFirstPath dfs = new DepthFirstPath(G, s);
+
+        for (int v = 0; v < G.V(); v++) {
+            if (dfs.hasPathTo(v)) {
+                StdOut.printf("%d to %d:  ", s, v);
+                for (int x : dfs.pathTo(v)) {
+                    if (x == s) StdOut.print(x);
+                    else        StdOut.print("-" + x);
+                }
+                StdOut.println();
+            }
+
+            else {
+                StdOut.printf("%d to %d:  not connected\n", s, v);
+            }
+
+        }
+
 
 }
