3c5fd0dc4f89dbd0a3aa7abd5cb28481b70c5e4a
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-01-04 23:29:40 +0200

    connected component

--- CS61B/Lectures/Graphs/ConnectedComponent.java
+++ CS61B/Lectures/Graphs/ConnectedComponent.java
@@ -73,5 +73,39 @@ public class ConnectedComponent {
         return id(v) == id(w);
     }
 
-    
+    public void validateVertix(int v) { 
+        int V = marked.length;
+        if(v < 0 || v >= V) { 
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Graph G = new Graph(in);
+        CC cc = new CC(G);
+
+        // number of connected components
+        int m = cc.count();
+        StdOut.println(m + " components");
+
+        // compute list of vertices in each connected component
+        Queue<Integer>[] components = (Queue<Integer>[]) new Queue[m];
+        for (int i = 0; i < m; i++) {
+            components[i] = new Queue<Integer>();
+        }
+        for (int v = 0; v < G.V(); v++) {
+            components[cc.id(v)].enqueue(v);
+        }
+
+        // print results
+        for (int i = 0; i < m; i++) {
+            for (int v : components[i]) {
+                StdOut.print(v + " ");
+            }
+            StdOut.println();
+        }
+    }
+
+
 }
