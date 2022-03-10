88d64ef3e7926f92a4a2defd2c5fa8a1dabfc6fd
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-03-10 21:33:42 +0200

    trie implemntation

--- CS61B/Lectures/Trie/TrieHT.java
+++ CS61B/Lectures/Trie/TrieHT.java
@@ -118,6 +118,26 @@ public class TrieHT<Value> {
         if ( d == query.length()) return length; 
         char c = query.charAt(d); 
         return longestPrefixOf(x.next[c], query, d+1, length);
+    } 
+
+    public void delete(String key) { 
+        if (key == null) throw new IllegalArgumentException(); 
+        root = delete(root, key, 0);
     }
 
+    private Node delete(Node x, String key, int d) { 
+        if (x == null) return null; 
+        if(d == key.length()) {  
+            if (x.val != null) N--; 
+            x.val = null;
+  
+        } else { 
+            char c = key.charAt(d); 
+            x.next[c] = delete(x.next[c], key, d+1); 
+        }
+        if (x.val != null) return x; 
+        for(char c = 0; c < R; c++) 
+            if (x.next[c] != null) return x; 
+        return null;
+    }
 }
