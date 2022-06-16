c6f47900cd990cead2de27d4abd4f879b84d5e37
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-06-16 23:07:12 +0200

    sql challenge

--- CS186/practice/practice.sql
+++ CS186/practice/practice.sql
@@ -149,9 +149,14 @@ where c.name = 'RED')
 
 
 
-/* */ 
+/* find daily active users */ 
+select activity_date as day, count(distinct user_id) as active_users 
+from activity 
+where datediff('2019-7-27', activity_date) < 30  and  activity_date <= '2019-07-27'
+group by activity_date
 
-/* */
+
+/* daily leads and partener */
 
 /* */
 
