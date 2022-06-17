37b49884f988fcfcab248e9630db3ea258ac7ba8
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-06-17 22:35:21 +0200

    sql challenge

--- CS186/practice/practice.sql
+++ CS186/practice/practice.sql
@@ -206,9 +206,12 @@ select stock_name, sum(
 from stocks 
 group by stock_name
 
-/* */
+/* sum of distance travlled */
 
-
+select u.name, ifnull(sum(r.distance), 0) as travelled_distance 
+from users u left join rides r on u.id = r.user_id 
+group by r.user_id 
+order by travelled_distance desc, name
 
 /* */
 
